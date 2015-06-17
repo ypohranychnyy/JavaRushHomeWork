7c73ab7b9330025ca07e5982aaf8a1fa1a939cbe
ROOT <the.whu@gmail.com>
2015-06-17 17:32:14 +0300

    inital commit

---
+++ src/com/javarush/test/level00/lesson02/task01/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level00.lesson02.task01;
+
+/* Быть программистом - круто!
+Напиши программу, которая выводит на экран надпись: «Я думаю, быть программистом - это круто».
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        //напишите тут ваш код
+        System.out.println("Я думаю, быть программистом - это круто");
+    }
+}
